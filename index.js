c04ad7e7f391897efcf5d4174f35b84d88cddfdb
lilei <lilei@qimingpian.com>
2018-04-26 00:49:25 +0800

    完成resolve

--- index.js
+++ index.js
@@ -4,20 +4,36 @@ module.exports = function Promise (fn) {
         deferreds = []
 
     this.then = function (onFulfilled) {
+        return new Promise(function (resolve) {
+            handle({
+                onFulfilled: onFulfilled || null,
+                resolve: resolve
+            })
+        })
+    }
+
+    function handle (deferred) {
         if (state === 'pending') {
-            deferreds.push(onFulfilled)
-            return this
+            deferreds.push(deferred)
+            return
         }
-        onFulfilled(value)
-        return this
+        var ret = deferred.onFulfilled(value)
+        deferred.resolve(ret)
     }
 
     function resolve (newValue) {
+        if (newValue && (typeof newValue === 'object' || typeof newValue === 'function')) {
+            var then = newValue.then
+            if (typeof then === 'function') {
+                then.call(newValue, resolve)
+                return
+            }
+        }
         value = newValue
         state = 'fulfilled'
         setTimeout(function () {
             deferreds.forEach(function (deferred) {
-                deferred(value)
+                handle(value)
             })
         }, 0)
     }
