e192ceffe7916a72247e90de2cbe21457eb191ae
lilei <lilei@qimingpian.com>
2018-04-26 00:36:47 +0800

    引入状态

--- demo/index.js
+++ demo/index.js
@@ -4,6 +4,7 @@ function getUserId () {
     return new llPromise(function (resolve) {
         setTimeout(function () {
             console.log('第一步')
+            resolve('第一步')
         }, 5000)
     })
 }
--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 module.exports = function Promise (fn) {
     var value = null,
+        state = 'pending',
         deferreds = []
 
     this.then = function (onFulfilled) {
-        deferreds.push(onFulfilled)
+        if (state === 'pending') {
+            deferreds.push(onFulfilled)
+            return this
+        }
+        onFulfilled(value)
+        return this
     }
 
-    function resolve (value) {
-        deferreds.forEach(function (deferred) {
-            deferred(value)
-        })
+    function resolve (newValue) {
+        value = newValue
+        state = 'fulfilled'
+        setTimeout(function () {
+            deferreds.forEach(function (deferred) {
+                deferred(value)
+            })
+        }, 0)
     }
 
     fn(resolve)
